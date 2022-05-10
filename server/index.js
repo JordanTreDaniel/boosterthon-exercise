9d7b059a807dd49f25fed784a9cb7d3e9b39a4bb
JordanTreDaniel <lilj390150@live.com>
2022-05-09 20:19:12 -0500

    Database to Mongo + Use nodemon

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 import express from "express";
-import mysql from "mysql2";
+import { MongoClient } from "mongodb";
 import cors from "cors";
 
-const db = mysql.createPool({
-  host: "mysql_db", // the host name MYSQL_DATABASE: node_mysql
-  user: "MYSQL_USER", // database user MYSQL_USER: MYSQL_USER
-  password: "MYSQL_PASSWORD", // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
-  database: "fundraisers", // database name MYSQL_HOST_IP: mysql_db
-});
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = process.env.MONGO_DB_CONN_STRING;
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("just connected", client.db);
+    const collections = await client.db.collections();
+    console.log({ collections });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
 
 const app = express();
 app.use(cors());
@@ -22,6 +31,31 @@ app.get("/test", (req, res) => {
   res
     .status(200)
     .json({ msg: "Jordan would be such a great employee to hire." });
+});
+
+app.get("/seedthedb", (req, res) => {
+  //create the fundraisers table
+
+  //create the review table
+  //randomly create 10 fundraisers
+  const adjectives = ["Funny", "Red", "Tangible", "Observant", "Dangerous"];
+  const nouns = ["Sanctuary", "Refuge", "Academy", "Place", "School"];
+  const fundraisers = [];
+  for (let i = 0; i < 10; i++) {
+    const getRandomElement = (arr) => {
+      const randomIdx = Math.floor(Math.random() * arr.length);
+      return arr[randomIdx];
+    };
+    fundraisers.push(
+      getRandomElement(adjectives) + " " + getRandomElement(nouns)
+    );
+  }
+  run().catch(console.dir);
+
+  //will there be resposne? get ids and then..
+  //from those ten fundraisers, randomly create 20 reviews
+  //insert into db
+  res.status(200).json({ msg: "It worked", fundraisers });
 });
 
 app.listen("3001", () => {});
